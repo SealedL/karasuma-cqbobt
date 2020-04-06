e66da861c96f9a77e90a2a43e25dcc5f0e171f6f
SealedL <wille@nerven.localdomain>
2020-04-06 23:37:15 +0800

    Correct Punctuation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Sisters.WudiLib;
@@ -17,9 +16,9 @@ namespace cqbot
 
             const string Help = "用法：/[命令] [参数(可选)]\n" +
                 "命令列表：\n" +
-                "help       显示本帮助内容；\n" +
-                "support    显示本项目GitHub仓库链接\n" +
-                "echo       复读命令后的内容\n";
+                "help       显示本帮助内容；\n\n" +
+                "support    显示本项目GitHub仓库链接;\n\n" +
+                "echo       复读命令后的内容。";
             const string Support = "本项目GitHub仓库链接：" +
                 "https://github.com/SealedL/karasuma-cqbot\n" +
                 "欢迎帮助我改进程序\n";

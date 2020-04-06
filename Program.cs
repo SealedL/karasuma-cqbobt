c590517c81aea70f56a06d2ac9cb8bec8164cc99
SealedL <wille@nerven.localdomain>
2020-04-06 23:25:40 +0800

    Clean Code

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Sisters.WudiLib;
 using Sisters.WudiLib.WebSocket;
 using Sisters.WudiLib.Posts;
-using Sisters.WudiLib.Responses;
 
 namespace cqbot
 {
@@ -13,15 +12,22 @@ namespace cqbot
     {
         static void Main(string[] args)
         {
+            const string HttpApiAdress = "http://127.0.0.1:5700/";
+            const string WebSocketEventAdress = "ws://127.0.0.1:6700/event";
 
-            string help = "使用方法：\n以\'/\'为开头加上功能列表里的功能命令和后续参数\n\n功能列表：\n1. echo：复读命令后的字符串；\n2. support：显示该项目GitHub仓库链接，欢迎贡献。\n";
-            string support = "本项目GitHub仓库链接：https://github.com/SealedL/karasuma-cqbot\n欢迎贡献";
-
+            const string Help = "用法：/[命令] [参数(可选)]\n" +
+                "命令列表：\n" +
+                "help       显示本帮助内容；\n" +
+                "support    显示本项目GitHub仓库链接\n" +
+                "echo       复读命令后的内容\n";
+            const string Support = "本项目GitHub仓库链接：" +
+                "https://github.com/SealedL/karasuma-cqbot\n" +
+                "欢迎帮助我改进程序\n";
 
             var httpApi = new HttpApiClient();
-            httpApi.ApiAddress = "http://127.0.0.1:5700/";
+            httpApi.ApiAddress = HttpApiAdress;
 
-            var webSocketEvent = new CqHttpWebSocketEvent("ws://127.0.0.1:6700/event");
+            var webSocketEvent = new CqHttpWebSocketEvent(WebSocketEventAdress);
             webSocketEvent.ApiClient = httpApi;
 
             // 订阅事件。
@@ -37,7 +43,7 @@ namespace cqbot
                         {
                             if (String.Compare(text, 0, "/help ", 0, 5) == 0)
                             {
-                                await httpApi.SendGroupMessageAsync(groupMessage.GroupId, help);
+                                await httpApi.SendGroupMessageAsync(groupMessage.GroupId, Help);
                             }
 
                             if (String.Compare(text, 0, "/echo ", 0, 5) == 0)
@@ -47,14 +53,13 @@ namespace cqbot
 
                             if (String.Compare(text, 0, "/support ", 0, 7) == 0)
                             {
-                                await httpApi.SendGroupMessageAsync(groupMessage.GroupId, support);
+                                await httpApi.SendGroupMessageAsync(groupMessage.GroupId, Support);
                             }
                         }
                     }
                 }
                 else if (e is PrivateMessage privateMessage)
                 {
-                    Console.WriteLine("QQ ID: " + privateMessage.UserId);
                 }
             };
 
@@ -81,7 +86,8 @@ namespace cqbot
                 while (true)
                 {
                     await Task.Delay(10000);
-                    Console.WriteLine("Available: {0}, Listening {1}", webSocketEvent.IsAvailable, webSocketEvent.IsListening);
+                    Console.WriteLine($"Available: {webSocketEvent.IsAvailable}," +
+                        $"Listening {webSocketEvent.IsListening}");
                 }
             });
 

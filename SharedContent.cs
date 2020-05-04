e4099b2506e69e1382334f5d147fe90ebea68e43
Wille <wille@nulladev.cn>
2020-05-04 17:18:54 +0800

    Complete help info

--- SharedContent.cs
+++ SharedContent.cs
@@ -14,18 +14,19 @@ namespace cqbot
         //Commands related strings
         public const string Help = "用法：/[命令] [参数(可选)]\n" +
                                    "命令列表：\n" +
-                                   "help       显示本帮助内容；\n\n" +
-                                   "support    显示本项目GitHub仓库链接;\n\n" +
-                                   "echo       复读命令后的内容；\n\n" + 
-                                   "comment    给作者留言。";
-        public const string Support = "本项目GitHub仓库链接：" +
+                                   "help [无参数] #显示本帮助内容；\n\n" +
+                                   "support [无参数] #显示本项目GitHub仓库链接;\n\n" +
+                                   "echo [string] #复读[string]；\n\n" + 
+                                   "comment [string] #给作者留言[string]；\n\n" + 
+                                   "wolfram [string] #利用Wolfram|Alpha搜索引擎对[string]进行计算。";
+        public const string Support = "本项目GitHub仓库链接：\n" +
                                       "https://github.com/SealedL/karasuma-cqbot\n" +
                                       "欢迎帮助我改进程序";
         public const string Comment = "您已成功送出一条留言";
         public const string SyntaxError = "格式错误";
         public const string Wait = "数据传输中，请耐心等待";
         public const string Error = "发生错误，请向管理员报告";
-        public const string Busy = "您有命令正在执行，🙏请等待命令执行完毕";
+        public const string Busy = "您有命令正在执行";
         
         //User ID related
         public static readonly long MasterId = ReadMasterId();

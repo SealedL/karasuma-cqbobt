f02908f99ddb98c036a9e75d08bd1ba600a7c5cd
Wille <wille@nulladev.cn>
2020-05-04 22:36:23 +0800

    Remove unused namespace

--- EventHandler.cs
+++ EventHandler.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web;
-using PuppeteerSharp;
 using Sisters.WudiLib;
 using Sisters.WudiLib.Posts;
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using PuppeteerSharp;
 using Sisters.WudiLib;
 using Sisters.WudiLib.WebSocket;
 

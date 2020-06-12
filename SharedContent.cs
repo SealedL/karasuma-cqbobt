2d906e5a07efa011d96898fc5a69da13782e33a2
Herz <herz@wille.localdomain>
2020-06-12 15:32:10 +0800

    Add Help Message Content

--- SharedContent.cs
+++ SharedContent.cs
@@ -16,7 +16,8 @@ namespace cqbot
                                    "support [无参数] #显示本项目GitHub仓库链接;\n\n" +
                                    "echo [string] #复读[string]；\n\n" + 
                                    "comment [string] #给作者留言[string]；\n\n" + 
-                                   "encode [string] #将[string]按URL链接的格式进行转码；\n\n" + 
+                                   "encode [string] #将[string]按URL链接的格式进行转义；\n\n" + 
+                                   "decode [string] #将[string]按URL链接转义字符进行反转义；" + 
                                    "wolfram [string] #利用Wolfram|Alpha搜索引擎对[string]进行计算。";
         public const string Support = "本项目GitHub仓库链接：\n" +
                                       "https://github.com/SealedL/karasuma-cqbot\n" +
